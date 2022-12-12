b12d4824f3d59e1cd5ed25d5add3bacfd20357d2
Yoann Maingon <yoann.maingon@mydatalinx.com>
2022-12-13 00:00:04 +0100

    fix loading data and copy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
+
+const columnDefs = [
+  { field: "elapsedTimeMs", sort: "desc" },
+  { field: "query", width: 500 },
+  { field: "time" },
+  { field: "allocatedBytes" },
+  { field: "dbid" },
+  { field: "event" },
+  { field: "executingUser" },
+  { field: "pageFaults" },
+  { field: "pageHits" },
+  { field: "runtime" },
+  { field: "type" },
+];
+const gridOptions = {
+  columnDefs: columnDefs,
+  rowData: [],
+  defaultColDef: { sortable: true, filter: true },
+  animateRows: true, // have rows animate to new positions when sorted
+  rowSelection: 'single',
+};
+let gridDiv
+document.addEventListener('DOMContentLoaded', () => {
+  gridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(gridDiv, gridOptions);
+  // on click copy the query to the clipboard
+  gridDiv.addEventListener('click', function (event) {
+    const selectedRow = gridOptions.api.getSelectedRows();
+    navigator.clipboard.writeText(selectedRow[0]?.query);
+  });
+});
+
+
 async function readLogFile() {
   // retrieve the fileInput
   const fileInput = document.getElementById('fileInput');
@@ -5,51 +38,15 @@ async function readLogFile() {
 
   // Creates a BlobReader object used to read `zipFileBlob`.
   const zipFileReader = new zip.BlobReader(zipFileBlob);
-
   const logWriter = new zip.TextWriter();
-
   const zipReader = new zip.ZipReader(zipFileReader);
   const firstEntry = (await zipReader.getEntries()).shift();
   const logText = await firstEntry.getData(logWriter);
   await zipReader.close();
 
-
   // parse logText into JSON
   const logJSON = JSON.parse(logText);
-
-  const columnDefs = [
-    { field: "elapsedTimeMs", sort: "desc" },
-    { field: "query", width: 500 },
-    { field: "time" },
-    { field: "allocatedBytes" },
-    { field: "dbid" },
-    { field: "event" },
-    { field: "executingUser" },
-    { field: "pageFaults" },
-    { field: "pageHits" },
-    { field: "runtime" },
-    { field: "type" },
-  ];
-
-
-  // let the grid know which columns and what data to use
-  const gridOptions = {
-    columnDefs: columnDefs,
-    rowData: logJSON,
-    defaultColDef: { sortable: true, filter: true },
-    animateRows: true, // have rows animate to new positions when sorted
-    rangeSelection: true, // allow range selection
-  };
-
-  const gridDiv = document.querySelector('#myGrid');
-  new agGrid.Grid(gridDiv, gridOptions);
-
-  // on click copy the query to the clipboard
-  gridDiv.addEventListener('click', function (event) {
-    const selectedRow = gridOptions.api.getSelectedRows()[0];
-    navigator.clipboard.writeText(selectedRow.query);
-
-  });
+  gridOptions.api.setRowData(logJSON);
 
 }
 
